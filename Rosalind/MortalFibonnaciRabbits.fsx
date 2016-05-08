530db2c2b4143ee82542b0f95f69a1a5c76c5a3f
Karol Sotomski <karol.sotomski@gmail.com>
2016-05-08 22:23:27 +0200

    Bunnies and Fibonacci

---
+++ Rosalind/MortalFibonnaciRabbits.fsx
@@ -0,0 +1,33 @@
+﻿// http://rosalind.info/problems/fibd/
+
+// Note: Below algorithm is super naive. I just wanted to try doing it iteratively.
+
+let f2 n m =
+    let ageByOneMonth bunnies = 
+        bunnies 
+        |> List.permute (fun idx -> (idx + 1) % bunnies.Length)
+        |> List.mapi (fun i x -> if i = 0 then 0UL else x)
+    
+    let breed bunnies =
+        let newBunnies = bunnies
+                            |> List.mapi (fun i x -> if i < 2 then 0UL else x)
+                            |> List.sum
+        bunnies
+        |> List.indexed
+        |> List.filter (fun (i, x) -> i <> 0)
+        |> List.map (fun (_, x) -> x)
+        |> List.append [newBunnies]
+
+    let liveOneMonth bunnies =
+        bunnies
+        |> ageByOneMonth
+        |> breed
+
+    let bunnies = [for c in [1..m+1] -> if c = 1 then 1UL else 0UL]
+
+    [2 .. n]
+    |> List.fold (fun bunnies x -> liveOneMonth bunnies) bunnies
+    |> List.take (bunnies.Length - 1) // Don't count the dead
+    |> List.sum
+
+f2 88 18 //4
---
+++ Rosalind/RabbitsAndRecurrenceRelations.fsx
@@ -0,0 +1,27 @@
+﻿// http://rosalind.info/problems/fib/
+
+// k = 3
+(*
+    month   newBunnies      oldBunnies      total
+    1       1               0               1
+    2       0               1               1
+    3       k               1               k+1
+    4       k               k+1             2k+1
+    5       k(k+1)          2k+1            k^2+3k+2
+    6       k(2k+1)         k^2+3k+2        
+    n       k*F(n-2)        F(n-1)          k*F(n-2) + F(n-1)
+*)
+
+open System
+
+let rec f (n:long) (k:long) : long =
+    match n with
+    | 1 -> 1
+    | 2 -> 1
+    | _ -> (f (n-1) k) + (k * (f (n-2) k))
+
+f 5 3
+
+f 40 5
+
+f 29 5
