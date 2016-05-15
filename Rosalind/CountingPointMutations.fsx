3ece313acaf804af562186c6ecf45f32038f76d6
Karol Sotomski <karol.sotomski@gmail.com>
2016-05-15 20:52:18 +0200

    Counting Point Mutations

---
+++ Rosalind/CountingPointMutations.fsx
@@ -0,0 +1,10 @@
+﻿// http://rosalind.info/problems/hamm/
+
+let src1 = "GAGCCTACTAACGGGAT"
+let src2 = "CATCGTAATGACGGCCT"
+let answer = 7
+
+Seq.zip src1 src2
+|> Seq.filter (fun (s1, s2) -> s1 <> s2)
+|> Seq.length
+
